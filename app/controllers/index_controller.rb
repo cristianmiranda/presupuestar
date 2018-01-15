f7bec67cf38480f63e24784438c791e6e3783274
Cristian Miranda <crism60@gmail.com>
2018-01-15 18:00:18 -0300

    Clase Persona

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,3 +1,13 @@
 class IndexController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def index
+    @person = Person.new
+    @person.age = 10
+    flash[:success] = @person.age
+  end
+  
+  def create
+    flash[:success] = 'Success!'
+  end
 end
