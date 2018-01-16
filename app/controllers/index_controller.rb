97337c473fe76654655e77b30cf17c4a64a35946
Cristian Miranda <crism60@gmail.com>
2018-01-15 21:01:46 -0300

    ThinReports integration

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,13 +7,27 @@ class IndexController < ActionController::Base
   
   def create
     @person = Person.new(person_params)
-    flash[:success] = @person.name
-    render :index
+    # flash[:success] = @person.name
+    report = Thinreports::Report.new layout: File.join(Rails.root, 'app', 'reports', 'presupuesto.tlf')
+    report.start_new_page do
+      item(:date).value(Time.now.strftime("%d/%m/%Y"))
+    end
+    
+    download_report(report)
   end
   
   private
     def person_params
       params.require(:person).permit(:name, :address, :city, :phone, :car, :patente, :chasis, :motor)
+    end
+    
+    def download_report(report)
+      filename = 'presupuesto.pdf'
+      report.generate(filename: filename)
+      File.open(filename, 'r') do |f|
+        send_data f.read.force_encoding('BINARY'), :filename => filename, :type => "application/pdf", :disposition => "attachment"
+      end
+      File.delete(filename)
     end
   
 end
